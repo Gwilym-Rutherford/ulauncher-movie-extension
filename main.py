852a6d486d8e4ef15b783c3ce49db5247ffe9316
Gwilym-Rutherford <58346802+Gwilym-Rutherford@users.noreply.github.com>
2025-05-31 21:09:43 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,185 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+
+import logging
+import requests
+import json
+import urllib.parse
+import subprocess
+
+
+logger = logging.getLogger(__name__)
+
+class MovieExtension(Extension):
+
+    def __init__(self):
+        super().__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventHandler())
+        
+    
+    def qualitySelection(data, movieName):
+        items = []
+        logger.debug(data)
+        MovieExtension.movieName = movieName
+        for info in data:
+            info = dict(info)
+            items.append(ExtensionResultItem(
+                icon='images/menu.png',
+                name=f"quality: {info["quality"]}",
+                description=f"type: {info["type"]} \t size: {info["size"]}",
+                on_enter=ExtensionCustomAction(
+                    {"function": "streamSelection",
+                     "data": info}, keep_app_open=True))
+            )
+            
+        return items
+    
+    def streamSelection(data, preferences):
+        text = ["Stream", "Download", "Close"]
+        description = ["Stream movie right now, save to:", "Download movie to:", "Close"]
+        items = []
+        for i in range(0, len(text)):
+            if text[i] == "Close":
+                items.append(ExtensionResultItem(
+                    icon='images/menu.png',
+                    name=text[i],
+                    description=description[i],
+                    on_enter=HideWindowAction())   
+                )
+            else:
+                if text[i] == "Download":
+                    obj = {"function": "download", "data": data}
+                else:
+                    obj = {"function": "stream", "data": data}
+                    
+                items.append(ExtensionResultItem(
+                    icon='images/menu.png',
+                    name=text[i],
+                    description=f"{description[i]} {preferences["download_path"]}",
+                    on_enter=ExtensionCustomAction(obj, keep_app_open=False))
+                )
+            
+        return items
+    
+    def build_magnet_uri(data, preferences):
+        magnet_uri = "magnet:?xt=urn:btih:"
+        magnet_uri += data["hash"]
+        magnet_uri += "&dn="
+        magnet_uri += urllib.parse.quote(MovieExtension.movieName)
+        
+        trackers = preferences["trackers"].replace(",", "&tr=")
+        magnet_uri += f"&tr={trackers}"
+        
+        return magnet_uri 
+    
+
+    def download(data, preferences):
+        magnet_uri = MovieExtension.build_magnet_uri(data, preferences)
+        subprocess.run(
+            f"webtorrent download '{magnet_uri}' --quiet --out '{preferences["download_path"]}'"
+            , shell=True
+        )
+        
+    def stream(data, preferences):
+        logger.debug(f"Data info: {data}")
+        logger.debug(f"Movie name: {MovieExtension.movieName}")
+        
+        magnet_uri = MovieExtension.build_magnet_uri(data, preferences)
+        logger.debug(f"magnet_uri: {magnet_uri}")
+        subprocess.run(
+            f"webtorrent '{magnet_uri}' --quiet --vlc --out '{preferences["download_path"]}'",
+            shell=True)
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def build_api_uri(self, movie_name, preferences):
+        api_uri = ""
+        
+        base_uri = "https://yts.mx/api/v2/list_movies.json"
+
+        # add movie name
+        api_uri = f"{base_uri}?query_term={urllib.parse.quote(str(movie_name))}"
+    
+        # apply all preferences to uri        
+        if preferences["order_by"]:
+            api_uri += f"&order_by={preferences["order_by"]}"
+            
+        if preferences["limit"]:
+            api_uri += f"&limit={preferences["limit"]}"
+            
+                
+        logger.debug(f"api uri: {api_uri}")
+
+        return api_uri
+    
+    def get_movies(self, api_uri, preferences):
+        response = json.loads(requests.get(api_uri).text)["data"]
+        all_movies = response["movies"]
+        
+        movies = dict()
+        for movie in all_movies:
+            movies[movie["title"]] = movie["torrents"]
+            
+        return movies
+
+
+    def on_event(self, event, extension):
+        
+        api_uri = self.build_api_uri(event.get_argument(), extension.preferences)
+        movies = self.get_movies(api_uri, extension.preferences)
+        
+        items = []
+        keys = list(movies.keys())
+        for movie in keys:
+            items.append(ExtensionResultItem(
+                icon='images/icon.png',
+                name=str(movie),
+                description=api_uri,
+                on_enter=ExtensionCustomAction(
+                    {"function": "quality",
+                    "data": movies[movie],
+                    "movieName": str(movie)}, keep_app_open=True))
+            )        
+
+        return RenderResultListAction(items)
+
+
+class ItemEnterEventHandler(EventListener):
+    
+    # quality show 
+    def on_event(self, event, extension):
+        
+        data = event.get_data()
+        
+        logger.debug(data)
+        
+        if data["function"] == "quality":
+            items = MovieExtension.qualitySelection(data["data"], data["movieName"])
+        elif data["function"] == "streamSelection":
+            items = MovieExtension.streamSelection(data["data"], extension.preferences)
+        elif data["function"] == "download":
+            MovieExtension.download(data["data"], extension.preferences)
+            return ExtensionCustomAction(data={}, keep_app_open=False)
+        elif data["function"] == "stream":
+            MovieExtension.stream(data["data"], extension.preferences)
+            return ExtensionCustomAction(data={}, keep_app_open=False)
+        else:
+            items = (ExtensionResultItem(
+                        icon='images/menu.png',
+                        name="Seems to be an error",
+                        description="You can try restaring",
+                        on_enter=HideWindowAction())
+                     )
+        
+        return RenderResultListAction(items)
+    
+        
+if __name__ == '__main__':
+    MovieExtension().run()
